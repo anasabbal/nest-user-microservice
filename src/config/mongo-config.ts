fd663ecc44d0947de2eba89d961897bf724bb43b
anasabbal <anas.abbal66@gmail.com>
2023-06-18 03:35:13 +0100

    init

--- src/config/mongo-config.ts
+++ src/config/mongo-config.ts
@@ -9,7 +9,7 @@ const configService = new ConfigService();
 export class MongoConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
     return {
-      uri: 'mongodb+srv://anasabbal10:zadina123@cluster0.hebr9ta.mongodb.net/user',
+      uri: configService.get('MONGO_URI'),
     };
   }
 }
